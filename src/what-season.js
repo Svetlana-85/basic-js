87b4eda2d2a802084c628f6a26c7d7e28ccdad3f
Svetlana-85 <svetlanka-85@yandex.ru>
2022-04-30 01:18:49 +0500

    Complete task Tower of Hanoi

--- src/what-season.js
+++ src/what-season.js
@@ -13,20 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(date) {
   throw new NotImplementedError('Not implemented');
-  try {
+  /*try {
     if (!date) return "Unable to determine the time of year!";
-    if (isNaN(Date.parse(date)))  ("Invalid date!");
-    if (!date.isValid()) return "Invalid date!";
+    if (date instanceof Date){
+      if (Object.prototype.toString.call(date) != '[Object Date]') throw new Error ("Invalid date!");
+  
+     // if (!date.getTime()) return "Invalid date!";
+      let month = date.getMonth();
+      if (month === 11 || month === 0 || month === 1) return "winter";
+      else if (month > 1 && month <=4) return "spring";
+      else if (month > 4 && month <=7) return "summer";
+      else if (month > 7 && month <=10) return "autumn";
+  } catch (error) {
+    return "Invalid date!";
+  }*/
+  //if (date === "no-value") return "Unable to determine the time of year!";
+  if (!date) return "Unable to determine the time of year!";
+  if (date instanceof Date){
+	console.log(Object.prototype.toString.call(date));
+    if (Object.prototype.toString.call(date) !== '[object Date]') return "Invalid date!";
+  try {
     let month = date.getMonth();
     if (month === 11 || month === 0 || month === 1) return "winter";
     else if (month > 1 && month <=4) return "spring";
     else if (month > 4 && month <=7) return "summer";
     else if (month > 7 && month <=10) return "autumn";
-    else return "Invalid date!";
-  } catch (error) {
-    return "Invalid date!";
+}
+  catch {
+	return "Invalid date!";
   }
-
+  }
+  return "Invalid date!";
 }
 
 module.exports = {
