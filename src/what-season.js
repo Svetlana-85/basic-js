885388be64c359944698c5e8551ced3178760463
Svetlana-85 <svetlanka-85@yandex.ru>
2022-05-02 15:00:04 +0500

    Complete task What-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,39 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(date) {
-  throw new NotImplementedError('Not implemented');
-  /*try {
-    if (!date) return "Unable to determine the time of year!";
-    if (date instanceof Date){
-      if (Object.prototype.toString.call(date) != '[Object Date]') throw new Error ("Invalid date!");
-  
-     // if (!date.getTime()) return "Invalid date!";
+function getSeason(date="nodate") {
+  //throw new NotImplementedError('Not implemented');
+  if (date === "nodate") return "Unable to determine the time of year!";
+  if (!date) throw new Error ("Unable to determine the time of year!");
+  if (date instanceof Date){
+	  //console.log(Object.prototype.toString.call(date));
+    try {
       let month = date.getMonth();
+      let time = date.getTime();
       if (month === 11 || month === 0 || month === 1) return "winter";
       else if (month > 1 && month <=4) return "spring";
       else if (month > 4 && month <=7) return "summer";
       else if (month > 7 && month <=10) return "autumn";
-  } catch (error) {
-    return "Invalid date!";
-  }*/
-  //if (date === "no-value") return "Unable to determine the time of year!";
-  if (!date) return "Unable to determine the time of year!";
-  if (date instanceof Date){
-	console.log(Object.prototype.toString.call(date));
-    if (Object.prototype.toString.call(date) !== '[object Date]') return "Invalid date!";
-  try {
-    let month = date.getMonth();
-    if (month === 11 || month === 0 || month === 1) return "winter";
-    else if (month > 1 && month <=4) return "spring";
-    else if (month > 4 && month <=7) return "summer";
-    else if (month > 7 && month <=10) return "autumn";
-}
-  catch {
-	return "Invalid date!";
+    }
+    catch {
+	    throw new Error ("Invalid date!");
+    }
   }
-  }
-  return "Invalid date!";
+  throw new Error ("Invalid date!");
 }
 
 module.exports = {
